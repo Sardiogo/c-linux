17efc41ce7b235b3fe82cc2371e92a18a107e9d5
Diogo Sardinha <diogosar@gmail.com>
2019-04-04 22:52:32 +0100

    add arrays operations example

--- c-utils/pointers-arrays_op.c
+++ c-utils/pointers-arrays_op.c
@@ -3,15 +3,17 @@
 #include <string.h>
 
 void exchange(float *a, float *b);
-void pointers_ex(void);
-void vectors_pointers_ex(void);
-void string_op_ex(void);
+void pointers_op_ex(void);
+void vectors_op_ex(void);
+void strings_op_ex(void);
+void arrays_op_ex(void);
 
 int main(void)
 {
-    pointers_ex();
-    vectors_pointers_ex();
-    string_op_ex();
+    pointers_op_ex();
+    vectors_op_ex();
+    strings_op_ex();
+    arrays_op_ex();
     return (0);
 }
 
@@ -25,12 +27,12 @@ void exchange(float *a, float *b)
 }
 
 /* Pointers operations example */
-void pointers_ex(void)
+void pointers_op_ex(void)
 {
     float y = 1.1, *ptr_y;
     float x = 1.2, *ptr_x, **ptr_ptr_x;
 
-    puts("\n***** void pointers_ex(void) *****\n");
+    puts("\n***** void pointers_op_ex(void) *****\n");
     printf("Before exchange  x=%f e y=%f\n", x, y);
     exchange(&x, &y);
     printf("After exchange x=%f e y=%f\n", x, y);
@@ -47,13 +49,13 @@ void pointers_ex(void)
 }
 
 /*Vectors and Pointers operations*/
-void vectors_pointers_ex(void)
+void vectors_op_ex(void)
 {
     int v[] = {11, 22, 33, 44, 55, 66, 77, 88, 99, 100};
     int i, *ptr2, *ptr = v;
     ptr2 = v; /*<=> ptr2 = &v*/
 
-    puts("\n***** void vector_pointers_ex(void) *****\n");
+    puts("\n***** void vectors_op_ex(void) *****\n");
     printf("&v=%p - v=%p - v[0]=%d - *v=%d\n", (void *)&v, (void *)v, v[0], *v);
     printf("&ptr=%p - ptr=%p - ptr[0]=%d - *ptr=%d\n", (void *)&ptr, (void *)ptr, ptr[0], *ptr);
     printf("&ptr2=%p - ptr2=%p - ptr2[0]=%d - *ptr2=%d\n", (void *)&ptr2, (void *)ptr2, ptr2[0], *ptr2);
@@ -72,13 +74,13 @@ void vectors_pointers_ex(void)
     puts("");
 }
 
-/*Strngs operations example*/
-void string_op_ex(void)
+/*Strings operations example*/
+void strings_op_ex(void)
 {
     char s[] = "Exams";
     char *ps = s, **pps = &ps;
 
-    puts("\n***** void string_op_ex(void) *****\n");
+    puts("\n***** void strings_op_ex(void) *****\n");
     printf("&s=%p - s=%p - s[0]=%c - *s=%c\n", (void *)&s, s, s[0], *s);
     printf("&ps=%p - ps=%p - ps[0]=%c - *ps=%c\n", (void *)&ps, ps, ps[0], *ps);
     printf("&pps=%p - pps=%p - *pps=%p - *pps[0]=%c - **pps=%c\n", (void *)&pps, (void *)pps, *pps, *pps[0], **pps);
@@ -88,4 +90,22 @@ void string_op_ex(void)
     printf("ps[4]=%c - *(ps+4)=%c - *ps+4=%c\n", ps[4], *(ps + 4), *ps + 4);
     printf("(*pps)[4]=%c - *((*pps)+4)=%c - **pps+4=%c\n", (*pps)[4], *((*pps) + 4), **pps + 4);
     puts("");
+}
+
+/*Arrays operations example*/
+void arrays_op_ex(void)
+{
+    int i, j;
+    int a[3][2] = {{11, 22}, {33, 44}, {55, 66}};
+
+    puts("\n***** void arrays_op_ex(void) *****\n");
+
+    puts("int a[3][2] = {{11, 22}, {33, 44}, {55, 66}}");
+    for (i=0; i<3; i++)
+        for (j=0; j<2; j++)
+            printf("\ta[%d][%d] = %d @ %p\n", i, j, a[i][j], (void *)(&a[i][j]));
+    printf("&a=%p - a=%p - a*=%p - **a=%d\n", (void *)&a, (void *)a, (void *)*a, **a);
+    printf("*a+1=%p - *(a+1)=%p - *(*(a+1))=%d - *(*a+1)=%d - **a+1=%d\n", (void *)(*a + 1), (void *)(*(a + 1)), *(*(a + 1)), *(*a + 1), **a + 1);
+    printf("a+1=%p - *(a+1)+1=%p - **(a+1)=%d - **(a+1)+1=%d - *(*a+1)+1=%d\n", (void *)(a+1), (void *)(*(a+1)+1), **(a+1), **(a+1)+1, *(*a+1)+1);
+    printf("a+2=%p - (*(a+2))+1=%p - *(*(a+2)+1)=%d - **(a+2)+1=%d - *(*a+2)+1=%d\n", (void *)(a+2), (void *)((*(a+2))+1), *(*(a+2)+1), **(a+2)+1, *(*a+2)+1);
 }
